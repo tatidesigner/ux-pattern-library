589b52714f5f125f616829de4ede331ff40c7639
Andy Armstrong <andya@edx.org>
2016-05-05 10:29:21 -0400

    Update 2016-05-05T14:29:17.045Z

--- static/edx-pattern-library/js/edx-icons.js
+++
@@ -1,9 +0,0 @@
-define(['afontgarde'],
-    function() {
-        'use strict';
-
-        window.AFontGarde('edx-icons', {
-            // Check a few random icons to see if our font loads
-            glyphs: '\uE621\uE622\uE623'
-        });
-    });
--- static/edx-pattern-library/js/pattern-library.js
+++
@@ -1,1 +0,0 @@
-require(["jquery","./ui.js","./dropdown-menu-example","edx-pattern-library/js/modernizr-custom","edx-pattern-library/js/afontgarde","edx-pattern-library/js/edx-icons"],function(r,e){});
